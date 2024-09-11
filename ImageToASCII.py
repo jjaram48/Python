11ddd01aff09aa48f9a4fff3503d2e9fb816aa73
Julian Jaramillo <jjaram48@gmail.com>
2024-09-11 15:32:42 +0100

    added numpy, averaged RGB values into grayscale_image

--- ImageToASCII.py
+++ ImageToASCII.py
@@ -1,13 +1,15 @@
 import cv2
+import numpy as np
 
 # Read the image
 image = cv2.imread("Image1.jpg")
 
 # Access pixel values
 # For pixel at (x, y)
-x = 21  # Replace with your desired x coordinate
-y = 21   # Replace with your desired y coordinate
+# x = 21  # Replace with your desired x coordinate
+# y = 21   # Replace with your desired y coordinate
 
-pixel_value = image[y, x]
+# pixel_value = image[y, x]
+grayscale_image = np.mean(image, axis=2)
 
-print(pixel_value)
+print(grayscale_image)
